8548fdf6b2e717214ec03c8c5c0c81d81646dcc7
mike <michaeljohngallo@gmail.com>
2013-07-25 22:35:48 +0100

    removed some print debugging commands

--- custom_registration/views.py
+++ custom_registration/views.py
@@ -12,15 +12,11 @@ from django.core.context_processors import csrf
 
 def register(request):
     if request.method == 'POST':
-    	print 'in post'
-    	print request.POST
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():
-            print 'form valid'
             new_user = form.save()
             new_group = GridGroup(founder=new_user)
             new_group.save()
-            print 'new user is saving'
             new_user = authenticate(username=request.POST['username'],
                                     password=request.POST['password1'])
             login(request, new_user)
