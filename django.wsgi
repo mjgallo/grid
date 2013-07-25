64055f4ee4d7fe6864af0dbef048e97b3efb0dac
mike gridique server <mike@gridique.com>
2013-07-25 12:28:04 +0000

    correct django.wsgi, but not serving static

--- django.wsgi
+++ django.wsgi
@@ -1,7 +1,11 @@
+activate_this = '/home/django/.virtualenvs/grid/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 import os, sys, site
 apache_configuration = os.path.dirname(__file__)
+print('APACHE CONFIG DIRNAME: %s' % apache_configuration)
 project = os.path.dirname(apache_configuration)
 workspace = os.path.dirname(project)
+sys.path.append(apache_configuration)
 sys.path.append(project)
 sys.path.append(workspace)
 import django
--- mysite/settings.py
+++ mysite/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'NAME': 'grid',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': 'marines',
-        'HOST': '/tmp/',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
