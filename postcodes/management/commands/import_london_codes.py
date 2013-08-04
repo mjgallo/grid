630467a4f50673f9b0a069c5354b51c075e05df4
mike <michaeljohngallo@gmail.com>
2013-08-04 22:20:42 +0100

    culled postcodes import file

---
+++ postcodes/management/commands/import_london_codes.py
@@ -0,0 +1,27 @@
+import sys, os
+import csv
+
+
+from django.contrib.gis.geos import Point
+from django.core.management.base import BaseCommand
+
+from postcodes.models import Postcode
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Postcode.objects.all().delete()
+        count = 0
+        with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'mycodes2.csv'))) as csvfile: 
+            for row in csv.reader(csvfile):
+                try:
+                    name = row[0].upper().strip().replace('  ', ' ')
+                    location = Point(map(float, row[1:3]))
+                except ValueError:
+                    print "I: skipping %r" % row
+                    continue
+
+                Postcode.objects.create(name=name, location=location)
+
+                count += 1
+                if count % 10000 == 0:
+                    print "Imported %d" % count
